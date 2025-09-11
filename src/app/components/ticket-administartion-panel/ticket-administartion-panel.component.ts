44e6ce2e12ebc56f0e734f3f513abf43a815c764
Pehuen <roldanpehuen@gmail.com>
2025-09-10 22:26:39 -0300

    feat: searchbar by ticket id

--- src/app/components/ticket-administartion-panel/ticket-administartion-panel.component.ts
+++ src/app/components/ticket-administartion-panel/ticket-administartion-panel.component.ts
@@ -10,7 +10,6 @@ import { TicketStatus } from '@app/interfaces/ticket_status.interface';
 import { User } from '@app/interfaces/user.interface';
 import { TicketSatusService } from '@app/services/ticket-satus.service';
 import { UserService } from '@app/services/user.service';
-
 @Component({
   selector: 'app-ticket-administartion-panel',
   templateUrl: './ticket-administartion-panel.component.html',
@@ -21,30 +20,25 @@ export class TicketAdministartionPanelComponent implements OnInit {
     return arg0.split('-')[0];
   }
   selectedTicketId: any;
-
   selectTicket(arg0: string) {
     this.selectedTicketId = arg0;
   }
-
   public user!: User;
-
+  public searchTicktId: string = '';
   public ticketList: Ticket[] = [];
   private statusList: TicketStatus[] = [];
   public showSpinner: boolean = true;
-
   public maxPagesCount: number = -1;
   public currentPageNumber: number = 1;
   public tableRowsLimit: number = 12;
-
   public filter: TicketFilterInput = {
     page: this.currentPageNumber,
     limit: this.tableRowsLimit,
+    ticket_id: this.searchTicktId ? this.searchTicktId : undefined,
   };
-
   get currentRolId() {
     return Number(this.user.role?.id) ?? -1;
   }
-
   constructor(
     @Inject(TICKET_SERVICE_INTERFACE_TOKEN)
     private ticketDataService: TicketServiceInterface,
@@ -52,55 +46,40 @@ export class TicketAdministartionPanelComponent implements OnInit {
     private router: Router,
     private ticketSatusService: TicketSatusService
   ) {}
-
   async ngOnInit(): Promise<void> {
     this.user = await this.userService.getUserData();
-
     this.ticketDataService.ticketCounter$.subscribe((max_tickets) => {
       this.maxPagesCount = Math.ceil(max_tickets / this.tableRowsLimit);
     });
-
     this.ticketDataService.ticketList$.subscribe((ticketList) => {
       this.ticketList = ticketList;
-
       this.showSpinner = false;
     });
-
     this.showSpinner = true;
-
     this.ticketDataService.UpdateTicketList(this.filter);
     this.ticketDataService.UpdateTicketCounter(this.filter);
   }
-
   goToMap() {
     this.router.navigate(['/map']);
   }
-
   formatDate(date: Date): string {
     const dateStr = date.toString();
-
     return dateStr.split('T')[0];
   }
-
   changePageTo(pageNumber: number) {
     this.showSpinner = true;
-
     if (pageNumber < 1) return;
-
     if (pageNumber > this.maxPagesCount) return;
-
     this.filter.page = pageNumber;
     this.currentPageNumber = pageNumber;
     this.ticketDataService.UpdateTicketList(this.filter);
   }
-
   get currentPagesNavigator(): number[] {
     if (this.currentPageNumber == 1 || this.currentPageNumber == 2) {
       const toReturn = [];
       for (let i = 0; i < 3; i++) {
         if (i + 1 <= this.maxPagesCount) toReturn.push(i + 1);
       }
-
       return toReturn;
     } else if (
       this.currentPageNumber == this.maxPagesCount ||
@@ -112,15 +91,21 @@ export class TicketAdministartionPanelComponent implements OnInit {
         this.maxPagesCount,
       ];
     }
-
     return [
       this.currentPageNumber - 1,
       this.currentPageNumber,
       this.currentPageNumber + 1,
     ];
   }
-
   onTicketUpdated($event: Event) {
     console.log('Ticket updated');
+  }
+  searchTicketById() {
+    this.showSpinner = true;
+    this.filter.page = 1;
+    this.currentPageNumber = 1;
+    this.filter.ticket_id = this.searchTicktId ? this.searchTicktId : undefined;
+    this.ticketDataService.UpdateTicketList(this.filter);
+    this.ticketDataService.UpdateTicketCounter(this.filter);
   }
 }
